0af48dcc2ab4fe5403e2a9a5cafa02b699739f6d
Fleur <163044507+fleursee@users.noreply.github.com>
2026-01-15 19:29:08 -0500

    fix: Update PrismaClient import to prepare for Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 // src/lib/prisma.ts
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({ 
